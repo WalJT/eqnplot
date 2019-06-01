e4045364ea2078ebf06c35cb6f6f0e952d09fcb8
WalJT <jttw259@zoho.com>
2019-06-01 12:34:31 +0100

    Added Test Entry Field and Button

--- main.py
+++ main.py
@@ -2,20 +2,30 @@
 
 import sys
 from PyQt5 import QtWidgets
+import matplotlib.pyplot as plt
 
 class EquationWindow(QtWidgets.QWidget):
 
     def __init__(self):
         super().__init__()
-
         self.init_ui()
 
     def init_ui(self):
 
-        self.setWindowTitle("Qquation Window")
+        self.eqn_box = QtWidgets.QLineEdit() # Equation Entry Box
+        self.plot_butt = QtWidgets.QPushButton("Plot Equation") # Button to plot equation
+
+        # Create Layout
+        vertical_layout = QtWidgets.QVBoxLayout()
+        vertical_layout.addWidget(self.eqn_box)
+        vertical_layout.addWidget(self.plot_butt)
+
+        self.setLayout(vertical_layout)
+        self.setWindowTitle("Equation Window")
 
         self.show()
 
 
 app = QtWidgets.QApplication(sys.argv)
 starting_window = EquationWindow()
+sys.exit(app.exec_())
