eb12a5be2f6840c88048051aa2b1e1f4bd9680d3
WalJT <waljt@pm.me>
2023-11-12 11:31:24 +0000

    Added basic UI elements

--- niceGUI_reimplementatin.py
+++ niceGUI_reimplementatin.py
@@ -1,4 +1,15 @@
 from nicegui import ui
 import matplotlib.pyplot as plt
-from numpy import *
+import numpy as np
 
+# Two rows containg equation and linmits for X axis
+with ui.column():
+    with ui.row():
+        ui.label("Enter an Equation:")
+        ui.input("Equation")
+    with ui.row():
+        ui.input("lower X")
+        ui.label("< x <")
+        ui.input("upper x")
+
+ui.run(title="eqnPlot")
