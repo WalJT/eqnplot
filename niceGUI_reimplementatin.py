43a9095da8de7d7ad16581c5c49b185a0a8ccc21
WalJT <waljt@pm.me>
2023-11-12 13:26:26 +0000

    Improve input labeling

--- niceGUI_reimplementatin.py
+++ niceGUI_reimplementatin.py
@@ -2,26 +2,25 @@ from nicegui import ui
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Two rows containg equation and linmits for X axis
 with ui.splitter() as splitter:
+    # Two rows containg equation and linmits for X axis
     with splitter.before:
         with ui.column():
             with ui.row():
-                ui.label("Enter an Equation:")
-                ui.input("Equation")
+                ui.input(label="Enter an Equation: y =", placeholder="x**2")
             with ui.row():
-                ui.input("lower X")
+                ui.input(label="Lower x limit:", placeholder="-10")
                 ui.label("< x <")
-                ui.input("upper x")
+                ui.input(label="Upper x limit:", placeholder="  10")
 
-        # Pacehoder column with sample plot
-        # Example taken from (https://nicegui.io/documentation/pyplot)
+    # Pacehoder column with sample plot
+    # Example taken from (https://nicegui.io/documentation/pyplot)
     with splitter.after:
         with ui.column():
-            with ui.pyplot(figsize=(3, 2)):
+            with ui.pyplot(figsize=(8, 5)):
                 x = np.linspace(0.0, 5.0)
                 y = np.cos(2 * np.pi * x) * np.exp(-x)
                 plt.plot(x, y, '-')
-    
 
+# Run the app
 ui.run(title="eqnPlot")
