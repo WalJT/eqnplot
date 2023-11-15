c3fd63891a5de10c9184b7547df958294d2f56aa
WalJT <waljt@pm.me>
2023-11-15 09:28:22 +0000

    Plot functiuon kinda working

--- niceGUI_reimplementatin.py
+++ niceGUI_reimplementatin.py
@@ -5,19 +5,19 @@ import numpy as np
 def plot_graph() -> None:
     with ui.column():
         with ui.pyplot(figsize=(8, 5)):
-            x = np.linspace(0.0, 5.0)
-            y = eval("x**2")
+            x = np.linspace(lower_x_input.value, upper_x_input.value)
+            y = eval(eqn_input.value)
             plt.plot(x, y, 'k-',)
 
 # Two rows containg equation and linmits for X axis
 with ui.column():
     with ui.row():
-        ui.input(label="Enter an Equation: y =", placeholder="x**2")
+        eqn_input = ui.input(label="Enter an Equation: y =", placeholder="x**2")
     with ui.row():
-        ui.input(label="Lower x limit:", placeholder="-10")
+        lower_x_input = ui.input(label="Lower x limit:", placeholder="-10")
         ui.label("< x <")
-        ui.input(label="Upper x limit:", placeholder="  10")
-    ui.button("Plot", on_click=plot_graph())
+        upper_x_input = ui.input(label="Upper x limit:", placeholder="  10")
+    ui.button("Plot", on_click=plot_graph)
 
 
 # Run the app
