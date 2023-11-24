080f456daea2f1f952adeb141a57a3557b2bff22
WalJT <waljt@pm.me>
2023-11-24 08:03:14 +0000

    formatting

--- niceGUI_reimplementatin.py
+++ niceGUI_reimplementatin.py
@@ -4,18 +4,18 @@ import numpy as np
 
 def plot_graph() -> None:
     try:
-        with ui.column():
-            with ui.pyplot(figsize=(8, 5)):
-                x = np.linspace(float(lower_x_input.value), float(upper_x_input.value))
-                y = eval(eqn_input.value)
-                plt.plot(x, y, 'k-',)
+        with ui.pyplot(figsize=(8, 5)):
+            x = np.linspace(float(lower_x_input.value), float(upper_x_input.value))
+            y = eval(eqn_input.value)
+            plt.plot(x, y, 'k-',)
     except:
         ui.notify("Error: Missing or invalid equation or parameters")
 
 # Two rows containg equation and linmits for X axis
 with ui.column():
     with ui.row():
-        eqn_input = ui.input(label="Enter an Equation: y =", placeholder="<an expression in terms of x>")
+        eqn_input = ui.input(label="Enter an Equation: y =",
+                             placeholder="<an expression in terms of x>")
     with ui.row():
         lower_x_input = ui.input(label="Lower x limit:", placeholder="<a number>")
         ui.label("< x <")
