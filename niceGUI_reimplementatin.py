f9b7478ab4b19a66c771e6cbc86b67e226fca3f6
WalJT <waljt@pm.me>
2023-11-24 07:41:39 +0000

    Display generic error on plot failure

--- niceGUI_reimplementatin.py
+++ niceGUI_reimplementatin.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_graph() -> None:
-    with ui.column():
-        with ui.pyplot(figsize=(8, 5)):
-            x = np.linspace(float(lower_x_input.value), float(upper_x_input.value))
-            y = eval(eqn_input.value)
-            plt.plot(x, y, 'k-',)
+    try:
+        with ui.column():
+            with ui.pyplot(figsize=(8, 5)):
+                x = np.linspace(float(lower_x_input.value), float(upper_x_input.value))
+                y = eval(eqn_input.value)
+                plt.plot(x, y, 'k-',)
+    except:
+        ui.notify("There was an error")
 
 # Two rows containg equation and linmits for X axis
 with ui.column():
